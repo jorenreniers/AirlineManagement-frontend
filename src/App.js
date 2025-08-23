9546727af00e887b00172d17610d33737de53ae3
reniers.J <renie@DESKTOP-E5F043U>
2025-08-23 19:55:20 +0200

    Aanpassingen in PassengerList.js en nieuwe navbar en pagelist css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import PassengerList from './components/PassengerList';
 import Navbar from './navbar/Navbar';
+import CreatePassenger from './components/CreatePassenger';
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
             <main className="app-main">
                 <Routes>
                     <Route path="/passengers" element={<PassengerList />} />
+                    <Route path="/createpassenger" element={<CreatePassenger/>}/>
                 </Routes>
             </main>
         </BrowserRouter>
--- src/components/CreatePassenger.js
+++ src/components/CreatePassenger.js
@@ -1,9 +1,9 @@
-import React, {useEffect, useState } from 'react';
+import React, {useState } from 'react';
 import "../styles/createPage.css"
 import"../styles/menuButtons.css"
-import {createPassenger} from '../services/PassengerService'
+import {createPassenger} from '../service/PassengerService'
 
-function AddPassenger() {
+function CreatePassenger() {
     const [formData, setFormData] = useState({
         name: '',
         firstName: '',
@@ -12,9 +12,45 @@ function AddPassenger() {
         phone: '',
         planeId: ''
     })
+
+    const handleChange = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value})
+    }
+
+    const handleCreatePassenger = ()=>{
+        createPassenger(formData)
+            .then(()=> alert("Passagier aangemaakt"))
+            .catch(e=> alert("Fout bij aanmaken passagier: " + e.message))
+    }
     return (
         <div>
+            <div className="Create-container">
+
+                <div className="Create-form">
+                    <h1>Nieuwe Passagier</h1>
+                    <div className="form-group">
+                        <label htmlFor="name">Naam:</label>
+                        <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required/>
+                        <label htmlFor="firstName">Voornaam:</label>
+                        <input type="text" id="firstName" name="firstName" value={formData.firstName} onChange={handleChange} required/>
+                        <label htmlFor="age">Leeftijd:</label>
+                        <input type="number" id="age" name="age" value={formData.age} onChange={handleChange} required/>
+                        <label htmlFor="email">E-mail:</label>
+                        <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required/>
+                        <label htmlFor="phone">Telefoon:</label>
+                        <input type="tel" id="phone" name="phone" value={formData.phone} onChange={handleChange} required/>
+                        <label htmlFor="planeId">Vliegtuig:</label>
+                        <input type="number" id="planeId" name="planeId" value={formData.planeId} onChange={handleChange} required/>
+                        <button onClick={handleCreatePassenger}>Aanmaken</button>
+
+
+                    </div>
+
+
+                    </div>
+                </div>
 
         </div>
     )
-}
+
+} export default CreatePassenger;
